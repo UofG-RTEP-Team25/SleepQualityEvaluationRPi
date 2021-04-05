26f9ff7a1dc7c8d266610b86f4d1ad4c456d0b89
zhuzhongju <707417786@qq.com>
2021-04-06 00:58:37 +0100

    enable movementsdetection

--- code/ADXL345/ADXL345.cpp
+++ code/ADXL345/ADXL345.cpp
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <iostream>
 #include "i2c.cpp"
+#include "ADXL345.h"
 
 using namespace std;
 
@@ -16,30 +17,42 @@ struct acc_dat{
 };
 
 
+void move_detect()
+{
+	int val = 0;
+	val =  i2c_read_reg8_singleByte(12 , 13 ,0xA6 , 0xA7 , INT_SOURCE);
+	val &= 0x18;
+	if((val & 0x18) == 0x10) cout << "active" << "\n";
+	else if((val & 0x18) == 0x08) cout << "inactive" << "\n";
+	else cout << "erro" << "\n";
+
+}
+
+
 void adxl345_init()
 {
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x31 , 0x0b);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x2d , 0x08);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x1e , 0x00);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x1f , 0x00);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x20 , 0x00);
-
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x21 , 0x00);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x22 , 0x00);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x23 , 0x00);
-
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x24 , 0x01);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x25 , 0x0f);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x26 , 0x2b);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x27 , 0x00);
+	i2c_read_reg8_singleByte(12 , 13 , 0XA6 , 0xA7 , INT_SOURCE) ;
+	i2c_write_reg8(12 , 13 , 0xA6 , INT_ENABLE , 0x00);
+	i2c_write_reg8(12 , 13 , 0xA6 , DATA_FORMAT , 0x0b);
+	i2c_write_reg8(12 , 13 , 0xA6 , POWER_CTL , 0x08);
+	i2c_write_reg8(12 , 13 , 0xA6 , BW_RATE , 0x0a);
 
 
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x28 , 0x09);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x29 , 0xff);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x2a , 0x80);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x2c , 0x0a);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x2f , 0x00);
-	i2c_write_reg8(12 , 13 , 0xA6 , 0x38 , 0x9f);
+	//enable movements detect
+	i2c_write_reg8(12 , 13 , 0xA6 , THRESH_ACT , 6);
+	i2c_write_reg8(12 , 13 , 0xA6 , THRESH_INACT , 4);
+	i2c_write_reg8(12 , 13 , 0xA6 , TIME_INACT , 1);
+	i2c_write_reg8(12 , 13 , 0xA6 , ACT_INACT_CTL , 0xff);
+
+
+	i2c_write_reg8(12 , 13 , 0xA6 , THRESH_FF , 0x09);
+	i2c_write_reg8(12 , 13 , 0xA6 , TIME_FF , 0x20);
+
+
+
+	i2c_write_reg8(12 , 13 , 0xA6 , INT_MAP , 0x00);
+	i2c_write_reg8(12 , 13 , 0xA6 , INT_ENABLE , 0x18);
+	i2c_write_reg8(12 , 13 , 0xA6 , FIFO_CTL , 0x9f);
 
 }
 
@@ -50,12 +63,12 @@ struct acc_dat adxl345_read_xyz()
 	char x0 , y0 , z0 , x1 , y1 , z1;
 	struct acc_dat acc_xyz;
 
-	x0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x32);
-	x1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x33);
-	y0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x34);
-	y1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x35);
-	z0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x36);
-	z1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , 0x37);
+	x0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAX0);
+	x1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAX1);
+	y0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAY0);
+	y1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAY1);
+	z0 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAZ0);
+	z1 = 0xff - i2c_read_reg8_singleByte(12 , 13 , 0xA6 , 0xA7 , DATAZ1);
 
 	acc_xyz.x = (int)(x1 << 8) + (int)x0;
 	acc_xyz.y = (int)(y1 << 8) + (int)y0;
@@ -75,12 +88,11 @@ int main()
 	wiringPiSetup();
 	i2c_init(12 , 13);
 	adxl345_init();
-
+	pinMode(30 ,INPUT);
+	pullUpDnControl(30 , PUD_DOWN);
+        if(wiringPiISR(30 , INT_EDGE_RISING , &move_detect) < 0) cout << "999";
 	while(1)
 	{
-		acc_xyz = adxl345_read_xyz();
-		cout << "  x: " << acc_xyz.x << "  y: " << acc_xyz.y << "  z: " << acc_xyz.z   << "\n";
 
-		delay(1000);
 	}
 }
---
+++ code/ADXL345/ADXL345.h
@@ -0,0 +1,40 @@
+#ifndef ADXL345_H_
+#define ADXL345_H_
+
+
+
+#define THRESH_TAP 0X1D
+#define OFSX       0X1E
+#define OFSY       0x1F
+#define OFSZ       0x20
+#define DUR        0x21
+#define Latent     0x22
+#define Window     0x23
+#define THRESH_ACT 0x24
+#define THRESH_INACT 0x25
+#define TIME_INACT 0x26
+#define ACT_INACT_CTL 0x27
+#define THRESH_FF     0x28
+#define TIME_FF       0x29
+#define TAP_AXES      0x2A
+#define ACT_TAP_STATUS 0x2B
+#define BW_RATE        0x2C
+#define POWER_CTL      0x2D
+#define INT_ENABLE     0x2E
+#define INT_MAP        0x2F
+#define INT_SOURCE     0x30
+#define DATA_FORMAT    0X31
+#define DATAX0         0x32
+#define DATAX1         0x33
+#define DATAY0         0x34
+#define DATAY1         0x35
+#define DATAZ0         0x36
+#define DATAZ1         0x37
+#define FIFO_CTL       0x38
+#define FIFO_STATUS    0x39
+
+
+
+
+#endif
+
