08c694772845dfdd4286094a0b83a12571d4a3b7
zhuzhongju <707417786@qq.com>
2021-04-12 00:31:48 +0100

    implement of ambient light detecting

---
+++ code/MAX44009_and_SoundSensor/MAX44009.cpp
@@ -0,0 +1,53 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <iostream>
+#include "i2c.cpp"
+#include "MAX44009.h"
+
+
+
+using namespace std;
+
+
+
+
+void max44009_init()
+{
+	i2c_read_reg8_singleByte( 21 , 22 , WRITE_ADDR_GND , READ_ADDR_GND , INTR_STATUS);
+
+	i2c_write_reg8( 21 , 22 , WRITE_ADDR_GND , REG_CONFIG , 0X80);
+	i2c_write_reg8( 21 , 22 , WRITE_ADDR_GND , INTR_ENABLE , 0x00);
+}
+
+
+void max44009_read_light()
+{
+	int i , lux , index = 0 , num = 0 , temp_num = 0 , sqrt = 1;
+
+	temp_num = i2c_read_reg8_singleByte( 21 , 22 , WRITE_ADDR_GND , READ_ADDR_GND , REG_LUX_HIGH);
+	index = (temp_num & 0xf0) >> 4;
+	for(i = 0;i < index;i++)
+	{
+		sqrt *= 2;
+	}
+	num = temp_num & 0x0f;
+	lux = sqrt * num * 0.72;
+	cout << index << "\n";
+	cout << num << "\n";
+	cout << "lux:" << lux <<"\n" << "\n";
+
+}
+
+int main()
+{
+	wiringPiSetup();
+	i2c_init( 21 , 22 );
+	max44009_init();
+	while(1)
+	{
+		max44009_read_light();
+		delay(2000);
+	}
+}
